9cf363a04ce702a0ca604926b06f5f53d602f558
rh2740 <rh2740@nyu.edu>
2018-11-21 16:34:05 -0500

    1

--- graph.py
+++ graph.py
@@ -63,30 +63,30 @@ def update(sloc, rdloc, ploc, oldmp, sz, repros, reprord, reprop, r):
                 offspring = np.clip(offspring, 0, 200)
                 nsloc.append(offspring)
     for c in ploc:
-        if mp[c[0], c[1], :].tolist() == [1, 1, 1]:
-            mp[c[0], c[1], 2] = 0
-            mp[c[0], c[1], 1] = 1
-            mp[c[0], c[1], 0] = 255
-            nploc.append(c)
-            for j in range(reprop):
-                rand = np.random.randint(low=-r, high=r, size=2)
-                x = c[0] + rand[0]
-                y = c[1] + rand[1]
-                offspring = np.asarray([x, y])
-                offspring = np.clip(offspring, 0, 200)
+        nploc.append(c)
+        mp[c[0], c[1], 2] = 0
+        mp[c[0], c[1], 1] = 1
+        mp[c[0], c[1], 0] = 255
+        for j in range(reprop):
+            rand = np.random.randint(low=-r, high=r, size=2)
+            x = c[0] + rand[0]
+            y = c[1] + rand[1]
+            offspring = np.asarray([x, y])
+            offspring = np.clip(offspring, 0, 200)
+            if mp[offspring[0], offspring[1], :].tolist() == [1, 1, 1]:
                 nploc.append(offspring)
     for b in rdloc:
-        if mp[b[0], b[1], :].tolist() == [1, 1, 1]:
-            mp[b[0], b[1], 2] = 0
-            mp[b[0], b[1], 1] = 255
-            mp[b[0], b[1], 0] = 0
-            nrdloc.append(b)
-            for j in range(reprord):
-                rand = np.random.randint(low=-r, high=r, size=2)
-                x = b[0] + rand[0]
-                y = b[1] + rand[1]
-                offspring = np.asarray([x, y])
-                offspring = np.clip(offspring, 0, 200)
+        nrdloc.append(b)
+        mp[b[0], b[1], 2] = 0
+        mp[b[0], b[1], 1] = 255
+        mp[b[0], b[1], 0] = 0
+        for j in range(reprord):
+            rand = np.random.randint(low=-r, high=r, size=2)
+            x = b[0] + rand[0]
+            y = b[1] + rand[1]
+            offspring = np.asarray([x, y])
+            offspring = np.clip(offspring, 0, 200)
+            if mp[offspring[0], offspring[1], :].tolist() == [1, 1, 1]:
                 nrdloc.append(offspring)
     return mp, nsloc, nploc, nrdloc
 
